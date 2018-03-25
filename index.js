92d09a579c1a843148c99dc541d990b7a7309f49
Omid <7697632+omidfi@users.noreply.github.com>
2018-03-25 08:00:50 +0300

    Add load test

---
+++ index.js
@@ -0,0 +1,29 @@
+var loadtest = require('loadtest');
+let noramlPageUrl = 'http://vagrant.local/?p=25';
+let pluginPageUrl = 'http://vagrant.local/?p=13&draftsforfriends=key_UnOHTTAm';
+var options = {
+    url: noramlPageUrl,
+    maxRequests: 1000,
+    concurrency: 10,
+    timelimit: 10,
+    requestsPerSecond: 40,
+    agentKeepAlive: true,
+    statusCallback
+};
+
+function statusCallback(error, result, latency) {
+    console.log('Current latency %j, error %j', latency,  error);
+    console.log('----');
+    console.log('Request elapsed milliseconds: ', result.requestElapsed);
+    console.log('Request index: ', result.requestIndex);
+}
+
+loadtest.loadTest(options, function(error, result)
+{
+    if (error)
+    {
+        return console.error('Got an error: %s', error);
+    }
+    console.log('Tests run successfully: ', result);
+});
+
